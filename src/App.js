bf8cec44c0971c524e49e092d2604b3c00daf5bf
Vedina2022 <2014vedina@gmail.com>
2022-09-19 17:13:34 -0600

    Added Dictionary  component and Search form inside it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
+      <div className="container">
+        <header className="App-header">
+          Dictionary
+          <div className="class"></div>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Nadiia Kyshinska</small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  function Search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={Search}>
+        <input type="search" onChange={handleKeywordChange} />
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+  );
+}
